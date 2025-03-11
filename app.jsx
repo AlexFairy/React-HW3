f599b596cbc075975cc020baad990f73adab6224
AlexFairy <AlexM121687@gmail.com>
2025-03-10 20:50:12 -0600

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CharacterList from './24. L3 HW/CharacterList';
+
+function App(){
+
+  return(
+    <>
+      <CharacterList />
+    </>
+  );
+}
+
+export default App
